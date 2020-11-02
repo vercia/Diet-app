22d7d757c187b3147336295d4df52130eae552a2
Weronika <werawera112@gmail.com>
2020-11-03 00:19:17 +0100

    Add AppContextProvider to App and delete unnecessary items

--- App.js
+++ App.js
@@ -4,53 +4,42 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './components/HomeScreen'
 import FoodScreen from './components/FoodScreen';
 import SettingsScreen from './components/SettingsScreen';
+import SingUpScreen from './components/SignUpScreen'
 import { Ionicons } from '@expo/vector-icons';
+import AppContextProvider from './components/AppContext'
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
+    <AppContextProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
 
-          if (route.name === 'Home') {
-            iconName = 'ios-home'
-          } else if (route.name === 'Food') {
-            iconName = 'ios-restaurant'
-          } else if (route.name === 'Settings') {
-            iconName = 'ios-settings'
-          }
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}>
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Food' component={FoodScreen} />
-        <Tab.Screen name='Settings' component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              if (route.name === 'Home') {
+                iconName = 'ios-home';
+              } else if (route.name === 'Food') {
+                iconName = 'ios-restaurant';
+              } else if (route.name === 'Settings') {
+                iconName = 'ios-settings';
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            }
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray'
+          }}
+        >
+          <Tab.Screen name='Home' component={HomeScreen} />
+          <Tab.Screen name='Food' component={FoodScreen} />
+          <Tab.Screen name='Settings' component={SettingsScreen} />
+          <Tab.Screen name='SignUp' component={SingUpScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </AppContextProvider>
   );
 }
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>TEST 123</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
