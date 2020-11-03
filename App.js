3911ccd9834907875aa592247897e92010452da7
Weronika <werawera112@gmail.com>
2020-11-03 22:23:11 +0100

    wrap all App with PaperProvider and add new navigator; create eatenMeal and new useEffect for progress

--- App.js
+++ App.js
@@ -1,45 +1,73 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './components/HomeScreen'
+import HomeScreen from './components/HomeScreen';
 import FoodScreen from './components/FoodScreen';
 import SettingsScreen from './components/SettingsScreen';
-import SingUpScreen from './components/SignUpScreen'
+import SingUpScreen from './components/SignUpScreen';
 import { Ionicons } from '@expo/vector-icons';
-import AppContextProvider from './components/AppContext'
+import AppContextProvider from './components/AppContext';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { createStackNavigator } from '@react-navigation/stack';
+import Profile from './components/Profile';
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
+const SettingsStack = createStackNavigator();
+function SettingsStackScreen() {
   return (
-    <AppContextProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size }) => {
-              let iconName;
-
-              if (route.name === 'Home') {
-                iconName = 'ios-home';
-              } else if (route.name === 'Food') {
-                iconName = 'ios-restaurant';
-              } else if (route.name === 'Settings') {
-                iconName = 'ios-settings';
-              }
-              return <Ionicons name={iconName} size={size} color={color} />;
-            }
-          })}
-          tabBarOptions={{
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray'
-          }}
-        >
-          <Tab.Screen name='Home' component={HomeScreen} />
-          <Tab.Screen name='Food' component={FoodScreen} />
-          <Tab.Screen name='Settings' component={SettingsScreen} />
-          <Tab.Screen name='SignUp' component={SingUpScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </AppContextProvider>
+    <SettingsStack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <SettingsStack.Screen name='Settings' component={SettingsScreen} />
+      <SettingsStack.Screen name='Profile' component={Profile} />
+    </SettingsStack.Navigator>
   );
 }
+
+export default function App() {
+  return (
+    <PaperProvider theme={theme}>
+      <AppContextProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+                if (route.name === 'Home') {
+                  iconName = 'ios-home';
+                } else if (route.name === 'Food') {
+                  iconName = 'ios-restaurant';
+                } else if (route.name === 'Settings') {
+                  iconName = 'ios-settings';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              }
+            })}
+            tabBarOptions={{
+              activeTintColor: 'tomato',
+              inactiveTintColor: 'gray'
+            }}
+          >
+            <Tab.Screen name='Home' component={HomeScreen} />
+            <Tab.Screen name='Food' component={FoodScreen} />
+            <Tab.Screen name='Settings' component={SettingsStackScreen} />
+            <Tab.Screen name='SignUp' component={SingUpScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </AppContextProvider>
+    </PaperProvider>
+  );
+}
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f'
+  }
+};
--- components/AppContext.js
+++ components/AppContext.js
@@ -8,13 +8,18 @@ export default function AppContextProvider(props) {
   const [gender, setGender] = useState('');
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
-  const [calories, setCalories] = useState(0)
+  const [calories, setCalories] = useState(0);
   const [carbos, setCarbos] = useState(0);
   const [proteins, setProteins] = useState(0);
   const [fats, setFats] = useState(0);
+  const [eatenCalories, setEatenCalories] = useState(0);
+  const [eatenCarbos, setEatenCarbos] = useState(0);
+  const [eatenProteins, setEatenProteins] = useState(0);
+  const [eatenFats, setEatenFats] = useState(0);
+  const [progress, setProgress] = useState(0)
 
-  const register = (nameText,ageText,genderText,weightText,heightText) => {
-    setName(nameText)
+  const register = (nameText, ageText, genderText, weightText, heightText) => {
+    setName(nameText);
     setAge(ageText);
     setGender(genderText);
     setWeight(weightText);
@@ -22,11 +27,22 @@ export default function AppContextProvider(props) {
   };
 
   useEffect(() => {
-    setCalories(Math.floor(66 + (13.7*weight)+(5*height)-(6.76*age)))
+    setCalories(Math.floor(66 + 13.7 * weight + 5 * height - 6.76 * age));
     setCarbos(Math.floor(weight * 1.5));
-    setProteins(Math.floor(weight*2))
-    setFats(Math.floor(weight * 1.2))
-  },[weight,age])
+    setProteins(Math.floor(weight * 2));
+    setFats(Math.floor(weight * 1.2));
+  }, [weight, age]);
+
+  useEffect(() => {
+    setProgress(eatenCalories/calories)
+  },[eatenCalories])
+
+  const eatenMeal = (calories, carbos, proteins, fats) => {
+    setEatenCalories(Math.floor(eatenCalories + calories));
+    setEatenCarbos(Math.floor(eatenCarbos + carbos));
+    setEatenProteins(Math.floor(eatenProteins + proteins));
+    setEatenFats(Math.floor(eatenFats + fats));
+  };
 
   return (
     <AppContext.Provider
@@ -40,7 +56,13 @@ export default function AppContextProvider(props) {
         calories,
         proteins,
         carbos,
-        fats
+        fats,
+        eatenCalories,
+        eatenCarbos,
+        eatenProteins,
+        eatenFats,
+        eatenMeal,
+        progress
       }}
     >
       {props.children}
