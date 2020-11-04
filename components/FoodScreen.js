aa48e9141b84e0d339be4033a0317a5d32214c58
Weronika <werawera112@gmail.com>
2020-11-04 23:47:38 +0100

    add API with recipes and map FoodCard

--- components/FoodScreen.js
+++ components/FoodScreen.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, FlatList } from 'react-native';
+import Axios from 'axios';
+import { Button, Searchbar } from 'react-native-paper';
+import FoodCard from './FoodCard';
 
 const FoodScreen = () => {
+  const [dataSource, setDataSource] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const APP_ID = '0da0cfb7b';
+  const APP_KEY = '0ba13508b45aa51cef52145df6da55605';
+
+  const getData = async () => {
+    const result = await Axios.get(
+      `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    setDataSource(result.data.hits);
+    console.log(result);
+  };
+
+  const onChangeSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const render = ({ item, index }) => {
+    return (
+      <FoodCard
+        key={item.recipe.label}
+        title={item.recipe.label}
+        image={item.recipe.image}
+        calories={item.recipe.calories}
+        carbos={item.recipe.totalNutrients.CHOCDF.quantity}
+        proteins={item.recipe.totalNutrients.PROCNT.quantity}
+        fats={item.recipe.totalNutrients.FAT.quantity}
+        ingredients={item.recipe.ingredientLines}
+      />
+    );
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Food!</Text>
+    <View style={{ flex: 1, padding: 24 }}>
+      <Searchbar
+        placeholder='Search'
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+      <Button onPress={getData}>Search</Button>
+      <FlatList
+        data={dataSource}
+        renderItem={render}
+        keyExtractor={(item, index) => index}
+      />
     </View>
   );
 };
