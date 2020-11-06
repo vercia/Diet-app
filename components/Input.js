04ed2c229fa872e6dbccaae487e53424bbff24dc
Weronika <werawera112@gmail.com>
2020-11-07 00:31:47 +0100

    Create one Input for two maps;

--- components/Input.js
+++ components/Input.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { TextInput } from 'react-native-paper';
 import styles from './Styles'
 
-const TextInputRegistration = (props) => {
+const Input = (props) => {
   return (
     <TextInput
       label={props.label}
+      keyboardType={props.keyboardType}
       value={props.value}
       onChangeText={props.onChangeText}
-      keyboardType={props.keyboardType}
-      secureTextEntry={props.secureTextEntry}
       style={styles.input}
+      secureTextEntry={props.secureTextEntry}
     />
   );
 };
 
-export default TextInputRegistration;
+export default Input;
