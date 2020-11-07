772ada41e22e15240540a798a503550255deba97
Weronika <werawera112@gmail.com>
2020-11-07 15:20:58 +0100

    istall expo-image-picker and add upload images in Profile

--- components/Profile.js
+++ components/Profile.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { Avatar, List } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 import { AppContext } from './AppContext';
 import DialogProfile from './DialogProfile';
 
@@ -19,6 +20,25 @@ const Profile = ({ navigation }) => {
   const [ageText, setAgeText] = useState();
   const [heightText, setHeightText] = useState();
   const [weightText, setWeightText] = useState();
+
+  const [photo, setPhoto] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setPhoto({ localUri: pickerResult.uri });
+  };
 
   const arrDialog = [
     {
@@ -49,31 +69,66 @@ const Profile = ({ navigation }) => {
 
   return (
     <View>
-      <Text onPress={() => navigation.navigate('Settings')}>
-        <Ionicons name='ios-arrow-back' size={50} color='black' />
+      <Text
+        style={{ color: 'white', fontSize: 20, padding: 15 }}
+        onPress={() => navigation.navigate('Settings')}
+      >
+        <Ionicons name='ios-arrow-back' size={20} color='white' />
         Back
       </Text>
-      <View>
-        <Avatar.Image size={140} source={require('../assets/favicon.png')} />
-        <Text>Name</Text>
+      <View style={{ alignItems: 'center' }}>
+        <View>
+          {photo == null ? (
+            <Avatar.Image size={140} source={require('../images/avatar.png')} />
+          ) : (
+            <Avatar.Image size={140} source={{ uri: photo.localUri }} />
+          )}
+
+          <Ionicons
+            style={{ position: 'absolute', bottom: 10, right: 10 }}
+            name='ios-add-circle'
+            size={32}
+            color='#00dfc0'
+            onPress={openImagePickerAsync}
+          />
+        </View>
       </View>
-      <List.Section>
-        <List.Subheader>Twój stary</List.Subheader>
-        <List.Item title={`Płeć: ${gender}`} />
-        {arrDialog.map((item) => {
-          return (
-            <DialogProfile
-              key={item.label}
-              title={item.title}
-              label={item.label}
-              keyboardType={item.keyboardType}
-              value={item.value}
-              onChangeText={item.onChangeText}
-              submitEdit={item.submitEdit}
-            />
-          );
-        })}
-      </List.Section>
+      <Text
+        style={{ color: 'white', fontSize: 20, top: 8, textAlign: 'center' }}
+      >
+        Name
+      </Text>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 30
+        }}
+      >
+        <List.Section
+          style={{
+            backgroundColor: 'rgba(210,207,206,.3)',
+            width: '90%',
+            borderRadius: 10
+          }}
+        >
+          <List.Subheader>Twoje dane</List.Subheader>
+          <List.Item title={`Płeć: ${gender}`} />
+          {arrDialog.map((item) => {
+            return (
+              <DialogProfile
+                key={item.label}
+                title={item.title}
+                label={item.label}
+                keyboardType={item.keyboardType}
+                value={item.value}
+                onChangeText={item.onChangeText}
+                submitEdit={item.submitEdit}
+              />
+            );
+          })}
+        </List.Section>
+      </View>
     </View>
   );
 };
