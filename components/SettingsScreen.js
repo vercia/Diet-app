47a7f1360423eb0875b54070a6f5a68d2ba56051
Weronika <werawera112@gmail.com>
2020-11-19 21:18:04 +0100

    edit SettingsScreen and change button with profile to listItem

--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Button } from 'react-native-paper'
-import styles from './Styles'
+import { Button, List, Divider } from 'react-native-paper';
+import styles from './Styles';
 
-const SettingsScreen = ({navigation}) => {
- return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button onPress={() => navigation.navigate('Profile')}>Profile</Button>
+const SettingsScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', top: 40 }}>
+      <List.Item
+        title='Profile'
+        titleStyle={{ color: '#00dfc0', fontSize: 20 }}
+        style={{ width: '90%' }}
+        onPress={() => navigation.navigate('Profile')}
+      />
+      <Divider style={{ backgroundColor: '#222', width: '90%' }} />
+      <List.Item
+        title='Profile'
+        titleStyle={{ color: '#00dfc0', fontSize: 20 }}
+        style={{ width: '90%' }}
+        // onPress={() => navigation.navigate('Profile')}
+      />
+      <Divider style={{ backgroundColor: '#222', width: '90%' }} />
+      <List.Item
+        title='Profile'
+        titleStyle={{ color: '#00dfc0', fontSize: 20 }}
+        style={{ width: '90%' }}
+        // onPress={() => navigation.navigate('Profile')}
+      />
+      <Divider style={{ backgroundColor: '#222', width: '90%' }} />
+      <List.Item
+        title='Profile'
+        titleStyle={{ color: '#00dfc0', fontSize: 20 }}
+        style={{ width: '90%' }}
+        // onPress={() => navigation.navigate('Profile')}
+      />
+      <Divider style={{ backgroundColor: '#222', width: '90%' }} />
       <Button
         style={styles.button}
         color='#5b2a83'
